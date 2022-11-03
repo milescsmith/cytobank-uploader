5370e9f5f54e75c1d605de0542fd0f1178118867
Miles Smith <mileschristiansmith@gmail.com>
2022-11-03 10:26:42 -0500

    test update for table widgets

--- src/cytobank_uploader/test.py
+++ src/cytobank_uploader/test.py
@@ -9,37 +9,50 @@
 
 # Uploader.run(log="Uploader.log")
 
-from textual.app import App
-from textual.widgets import Placeholder
+    def on_click(self, event) -> None:
+        if event.focused.sender.id == "exp-tbl":
+            self.selectedexp = event.sender.focused.contents
+
+        self.selectedfile = event.path
+
+    
 
 
-class GridTest(App):
-    async def on_mount(self) -> None:
-        """Make a simple grid arrangement."""
+from textual.app import App, ComposeResult
+from textual.layout import Vertical
+from textual.widgets import Static, Header
 
-        grid = await self.view.dock_grid(edge="left", name="left")
+from cytobank_uploader.widgets import ClickableDataTable
 
-        grid.add_column(fraction=1, name="left", min_size=20)
-        grid.add_column(size=30, name="center")
-        grid.add_column(fraction=1, name="right")
 
-        grid.add_row(fraction=1, name="top", min_size=2)
-        grid.add_row(fraction=2, name="middle")
-        grid.add_row(fraction=1, name="bottom")
+class TableTest(App):
+    BINDINGS = [
+        ("q", "quit", "Quit"),
+    ]
 
-        grid.add_areas(
-            area1="left,top-start|middle-end", #"left,top",
-            area2="center,middle",
-            area3="left-start|right-end,bottom",
-            area4="right,top-start|middle-end",
+    def on_mount(self):
+        tbl = self.query_one("#click-tbl", ClickableDataTable)
+        tbl.add_column("One")
+        tbl.add_column("Two")
+        tbl.add_row("une", "deux")
+        tbl.add_row("eins", "zwei")
+        tbl.add_row("uno", "dos")
+
+
+    def on_click(self, event: ClickableDataTable.CellClicked) -> None:
+        text_bx = self.query_one("#textbox", Static)
+        text_bx.update(event.sender.focused.contents)
+
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Vertical(
+            Static("Nothing", id="textbox"),
+            ClickableDataTable(id="click-tbl"),
+            id="widget-vert"
         )
 
-        grid.place(
-            area1=Placeholder(name="area1"),
-            area2=Placeholder(name="area2"),
-            area3=Placeholder(name="area3"),
-            area4=Placeholder(name="area4"),
-        )
+tabletest = TableTest(title="Table Test", watch_css=True)
 
-
-GridTest.run(title="Grid Test", log="textual.log")
+if __name__ == "__main__":
+    tabletest.run()
